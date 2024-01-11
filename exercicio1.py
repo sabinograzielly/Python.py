9ebfea99937e0ec49725227b5a76a4e65f4ab213
sabinograzielly <grzllsabino@gmail.com>
2024-01-11 12:14:58 -0300

    .

---
+++ exercicio1.py
@@ -0,0 +1,24 @@
+number = input("Dial a number")
+
+if number == '0':
+    print("Entered 0")
+elif number == '1':
+    print("Entered 1")
+elif number == '2':
+    print("Entered 2")
+elif number == '3':
+    print("Entered 3")
+elif number == '4':
+    print("Entered 4")
+elif number == '5':
+    print("Entered 5")
+elif number == '6':
+    print("Entered 6")
+elif number == '7':
+    print("Entered 7")
+elif number == '8':
+    print("Entered 8")
+elif number == '9':
+    print("Entered 9")
+else:
+    print("Invalid number!")
